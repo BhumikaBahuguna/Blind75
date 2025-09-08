633325428bef0965a8738434b3d92c5e12265198
BHUMIKA BAHUGUNA <158489676+BhumikaBahuguna@users.noreply.github.com>
2025-09-08 22:58:13 +0530

    Add files via upload

---
+++ maxsumofnonadj3.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int maxSumNonAdjacent(vector<int>&nums){
+    int n=nums.size();
+    int prev=nums[0];
+    int prev2=0;
+    for(int i=1;i<n;i++){
+        int take=nums[i];
+        if(i>1) take+=prev2;
+        int nontake=0+prev;
+        int curi=max(take,nontake);
+        prev2=prev;
+        prev=curi;
+    }
+    return prev;
+}
+int main(){
+    int n;
+    cin>>n;
+    vector<int>nums(n);
+    for(int i=0;i<n;i++) cin>>nums[i];
+    cout<<maxSumNonAdjacent(nums)<<endl;
+    return 0;
+}
